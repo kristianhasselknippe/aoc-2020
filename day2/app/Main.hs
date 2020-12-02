349beac78baf110ded5dcac5eb4d7f7ca10a08f8
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2020-12-02 18:55:56 +0100

    Day 2 part 2

--- day2/app/Main.hs
+++ day2/app/Main.hs
@@ -12,7 +12,7 @@ import Text.Read
 --   read (a,b) = "(" ++ a ++ ", " ++ b ++ ")"
 
 data Policy = Policy {
-  range :: (Integer, Integer),
+  range :: (Int, Int),
   letter :: Char,
   value :: String
 }
@@ -22,7 +22,7 @@ instance Show Policy where
 parseLine :: String -> Policy
 parseLine line =
   let parts = splitOn " " line
-      range = let [a,b] = (map read (splitOn "-" (parts !! 0)) :: [Integer])
+      range = let [a,b] = (map read (splitOn "-" (parts !! 0)) :: [Int])
               in (a,b)
       letter = parts !! 1 !! 0 :: Char
       value = parts !! 2
@@ -43,14 +43,32 @@ validate p =
               (value p)
   in low <= counted && counted <= high
 
+safeIndex :: [a] -> Int -> Maybe a
+safeIndex l i
+       | i >= 0 && i < length l = Just (l !! i)
+       | otherwise = Nothing
+
+validatePart2 :: Policy -> Bool
+validatePart2 p =
+  let
+      theLetter = letter p
+      (first, second) = range p
+      firstLetter = safeIndex (value p) (first-1)
+      secondLetter = safeIndex (value p) (second-1)
+  in case (firstLetter, secondLetter) of
+          (Just f, Just s) -> ((f == theLetter) || (s == theLetter)) && f /= s
+          (Just f, Nothing) -> f == theLetter
+          (Nothing, Just s) -> s == theLetter
+          (Nothing, Nothing) -> False
+
 main :: IO ()
 main = do
   currentDir <- getCurrentDirectory
   print currentDir
   fileContent <- readFile (currentDir ++ "/../input-sets/day2-1.txt")
-  print fileContent
+  --print fileContent
   let lines  = filter (\x -> (length x) > 0) (splitOn "\n" fileContent)
       policies = map parseLine lines
-      validated = filter (\x -> x == True) (map validate policies)
+      validated = filter id (map validatePart2 policies)
       numValidated = length validated
   print ("Number of valid: " ++ (show numValidated))
