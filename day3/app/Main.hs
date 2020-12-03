f8b42997cd181979eed9a7700529cf60a0f7d9d1
Kristian Hasselknippe <kristian@nortero.com>
2020-12-03 22:02:05 +0100

    Day3 part 2

--- day3/app/Main.hs
+++ day3/app/Main.hs
@@ -6,7 +6,6 @@ import System.IO
 import System.Directory
 import Data.List
 import Data.List.Split
-import Debug.Trace
 import Text.Read
 import System.Environment(getArgs, getProgName)
 
@@ -18,14 +17,15 @@ parseInput path = do
 hasTree :: String -> Int -> Bool
 hasTree line xPos =
   let lineLen = length line
-      index = (trace ("XPos: " ++ show xPos) xPos) `mod` (trace ("Line len: " ++ show lineLen) lineLen)
-  in (line !! (traceShow index index)) == '#'
+      index = xPos `mod` lineLen
+  in (line !! index) == '#'
 
-countTreesInPath :: [String] -> Int
-countTreesInPath lines =
-  sum $ map (\x -> case x of
+countTreesInPath :: [String] -> (Int, Int) -> Int
+countTreesInPath lines (right, down) =
+  let linesInPath = map snd $ filter (\(i,l) -> i `mod` down == 0) $ (zip [0..] lines)
+  in sum $ map (\x -> case x of
                    True -> 1
-                   False -> 0) $ zipWith hasTree lines [x * 3 | x <- [0..]]
+                   False -> 0) $ zipWith hasTree linesInPath [x * right | x <- [0..]]
 
 main :: IO ()
 main = do
@@ -33,5 +33,9 @@ main = do
   print argv
   input <- parseInput (argv !! 0)
   print input
-  let result = countTreesInPath $ (filter (\l -> length l > 0)) $ (splitOn "\n" input)
-  print ("Result: " ++ (show result))
+  let lines = (filter (\l -> length l > 0)) $ (splitOn "\n" input)
+  let resultP1 = countTreesInPath lines (3, 1)
+  print ("Result p1: " ++ (show resultP1))
+  let paths = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+  let resultP2 = product $ map (countTreesInPath lines) paths
+  print ("result p2: " ++ (show resultP2))
