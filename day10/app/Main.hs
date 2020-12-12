3f230a60742949dbfa0a393706f5bd6df12b8861
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2020-12-12 17:59:29 +0100

    Almost day 10

---
+++ day10/app/Main.hs
@@ -0,0 +1,83 @@
+module Main where
+
+import System.Directory
+import System.Environment
+import Text.Parsec
+import Flow
+import qualified Data.Map as Map
+import Text.ParserCombinators.Parsec.Prim(Parser)
+import Data.List.Split
+import qualified Data.Set as Set
+import Debug.Trace
+import Data.List
+import Data.Sort
+
+
+part1 :: [Int] -> [Int] -> [Int]
+part1 result (a1:a2:rest) = result ++ [abs (a2 - a1)] ++ part1 result (a2:rest)
+part1 result [last] = result ++ [3]
+part1 result [] = result ++ [3]
+
+
+isBetween :: Int -> Int -> Int -> Bool
+isBetween x a b =
+  x >= a && x <= b
+
+validConnection :: Int -> Int -> Bool
+validConnection a b = isBetween (abs (b - a)) 1 3
+
+type Connections = Map.Map Int [Int]
+
+validConnections :: [Int] -> [Int]
+validConnections (a:rest) = do
+  rest |> takeWhile (\x -> validConnection a x)
+
+buildGraph :: Connections -> [Int] -> Connections
+buildGraph c [] = c
+buildGraph connections l =
+  let (h:rest) = l
+      nextItems = validConnections l
+      ret = Map.insert h nextItems connections
+  in buildGraph ret rest
+
+countPathsInGraph :: Connections -> Set.Set Int -> Int -> Int
+countPathsInGraph graph visited from =
+   let targets = traceShow visited (graph Map.! from)
+   in case targets of
+        [] -> 1
+        _ -> targets
+               |> filter (\x -> Set.notMember x visited)
+               |> map (\x -> countPathsInGraph graph (Set.insert x visited) x)
+               |> sum
+
+part2 list =
+        let graph = buildGraph Map.empty list
+        in trace ("Counting paths for graph: " ++ show graph) (countPathsInGraph graph Set.empty 0)
+
+
+main :: IO ()
+main = do
+  argv <- getArgs
+  let path = head argv
+  currentDir <- getCurrentDirectory
+  content <- readFile $ currentDir ++ "/" ++ path
+  let input = [0] ++ (content |> splitOn "\n" |> filter (not . null) |> map read :: [Int])
+  print input
+  --let p1res = input |> sort |> part1 [] |> sort |> group |> map (\g -> ((g, length g)))
+
+  let testInput = [0, 16 ,10 ,15 ,5 ,1 ,11 ,7 ,19 ,6 ,12 ,4] |> sort
+  let testInput2 = [0, 28 ,33 ,18 ,42 ,31 ,14 ,46 ,20 ,48 ,47 ,24 ,23 ,49 ,45 ,19 ,38 ,39 ,11 ,1 ,32 ,25 ,35 ,8 ,17 ,7 ,9 ,4 ,2 ,34 ,10 ,3] |> sort
+  --let testRes = testInput |> sort |> part1 [] |> sort |> group |> map (\g -> ((g, length g)))
+  --print ("Part 1 result: " ++ show p1res)
+
+  print ("Test 2 inp: " ++ show testInput2)
+  let t1res = testInput |> sort |> part2
+  print ("Part 2 test 1 result: " ++ show t1res)
+
+  let t2res = testInput2 |> sort |> part2
+  print ("Part 2 test 2 result: " ++ show t2res)
+
+  let sortedInput = input |> sort
+  print ("Input: " ++ show sortedInput)
+  let p2res = sortedInput |> part2
+  print ("Part 2 result: " ++ show p2res)
