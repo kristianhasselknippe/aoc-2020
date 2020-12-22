12c47310ee4feddd9b8fef970d27d9eea72e6b6a
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2020-12-22 13:06:22 +0100

    Day 11 part 1

--- day11/app/Main.hs
+++ day11/app/Main.hs
@@ -10,6 +10,7 @@ import Data.List.Split
 import qualified Data.Set as Set
 import Debug.Trace
 import Data.List
+import Data.Array
 import Data.Sort
 
 step :: Char -> [Char] -> Char
@@ -24,16 +25,43 @@ step seat neighbors =
       '.' -> '.'
 
 
+neighborhood x y w h =
+  [(i + x, j + y) | i <- [-1..1], j <- [-1..1]]
+      |> filter (\(x,y) -> x >= 0 && x < w && y >= 0 && y < h) |> delete (x,y)
 
-gatherNeighbors :: [[Char]] -> Int -> Int -> Int -> Int -> [Char]
-gatherNeighbors seating w h x y =
-  let area = [(i + x, j + y) | i <- [-1..1], j <- [-1..1]] |> filter (\(x,y) -> x >= 0 && x < w && y >= 0 && y < h)
-  in trace ("Area: " ++ show area) area |> map (\(i,j) -> trace ("X: " ++ show i ++ " y: " ++ show j) seating !! j !! i)
+
+gatherNeighbors :: Array Int Char -> (Int, Int) -> (Int, Int) -> [Char]
+gatherNeighbors seating (w, h) (x, y) =
+  let n = neighborhood x y w h
+  in n |> map (\(x,y) -> seating ! (w * y + x))
+
+part1Update :: (Int, Int) -> Array Int Char -> Array Int Char
+part1Update (w, h) input =
+   [(x,y) | y <- [0..h-1], x <- [0..w-1]]
+        |> map (\(x,y) -> let ns = gatherNeighbors input (w, h) (x, y)
+                          --in step (input ! (trace ("WH: " ++ show (w,h) ++ ", XY: " ++ show (x,y) ++ ", I: " ++ show (y * w + x)) (y * w + x))) ns
+                          in step (input ! (y * w + x)) ns
+      )
+        |> listArray (0, w * h - 1)
+
+simulate :: (Int, Int) -> Array Int Char -> Array Int Char
+simulate size input =
+   let newBoard = part1Update size input
+   in if (trace ("\nBO: " ++ show (newBoard |> elems)) newBoard) /= input
+   --in if newBoard /= input
+     then simulate size newBoard
+     else newBoard
+
+countOccupied :: Array Int Char -> Int
+countOccupied input =
+    input |> elems |> filter ('#' ==) |> length
 
 part1 input =
-  let h = trace ("Height: " ++ show (length input)) length input
-      w = trace ("Width: " ++ show (length (head input))) (length (head input))
-  in [(x,y) | x <- [0..w], y <- [0..h]] |> map (\(x,y) -> let ns = gatherNeighbors input w h x y in step (input !! y !! x) (trace ("NS: " ++ show ns) ns))
+   let h = length input
+       w = length (head input)
+       arrayInput = (trace ("W: " ++ show w ++ ", H: " ++ show h ++ " Len: " ++ show (w * h)) listArray) (0, w * h - 1) (input |> concat)
+       res = simulate (w, h) arrayInput
+   in countOccupied res
 
 
 main :: IO ()
@@ -42,7 +70,8 @@ main = do
   let path = head argv
   currentDir <- getCurrentDirectory
   content <- readFile $ currentDir ++ "/" ++ path
-  let input = content |> splitOn "\n" |> filter (not . null)
+  let input = content |> splitOn "\n" |> filter (not . null) |> map (filter (/='\r'))
+  print ("Input: " ++ show input)
   let resP1 = part1 input
   print ("Part 1 result: " ++ show resP1)
   --print content
